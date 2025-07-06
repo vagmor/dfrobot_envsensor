4ec3c855bf5bc8074f7f1574733ea10338bf4ebb
vagmor <vag@tothema.net>
2025-07-06 16:27:23 +0300

    Add files via upload

--- esphome/components/dfrobot_envsensor/dfrobot_envsensor.h
+++ esphome/components/dfrobot_envsensor/dfrobot_envsensor.h
@@ -3,6 +3,8 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "DFRobot_EnvironmentalSensor.h"
+#include "esphome/components/i2c/i2c.h"
+
 
 namespace esphome {
 namespace dfrobot_envsensor {
@@ -21,9 +23,15 @@ class DFRobotEnvSensor : public PollingComponent {
   void set_light_sensor(sensor::Sensor *sensor) { light_sensor_ = sensor; }
   void set_uv_index_sensor(sensor::Sensor *sensor) { uv_index_sensor_ = sensor; }
   void set_elevation_sensor(sensor::Sensor *sensor) { elevation_sensor_ = sensor; }
+  void set_i2c_bus(esphome::i2c::I2CBus *bus) { this->i2c_bus_ = bus; }
+  void set_i2c_address(uint8_t address) { this->address_ = address; }
+
 
  protected:
   DFRobot_EnvironmentalSensor *sensor_{nullptr};
+  esphome::i2c::I2CBus *i2c_bus_{nullptr};
+  uint8_t address_;
+
 
   sensor::Sensor *temperature_sensor_{nullptr};
   sensor::Sensor *humidity_sensor_{nullptr};
